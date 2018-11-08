5b1cd0ec1ddb71dd2528bc47441e66fb64cfb534
Shannon Harris <sr60621@gmail.com>
2018-11-07 19:21:42 -0600

    Test two: name defined and not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        //URL defined and not empty
          it('url defined', function() {
              for(let feed of allFeeds) {
                  expect(feed.url).toBeDefined();
@@ -43,6 +43,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         //Name defined and not empty
+         it('name defined', function() {
+             allFeeds.forEach(function(feed) {
+                 expect(feed.name).toBeDefined();
+                 expect(feed.name.length).not.toBe(0);
+             });
+         });
     });
 
 
