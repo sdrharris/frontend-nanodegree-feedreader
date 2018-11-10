2977357c5db58a2191c55cc2c0921e6e8a10f522
Shannon Harris <sr60621@gmail.com>
2018-11-10 13:44:43 -0600

    Test for new feed function

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -102,9 +102,27 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() { 
+        const feed = document.querySelector('.feed');
+        const firstFeed = []; // first feed content empty array variable
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+            loadFeed(0);
+            Array.from(feed.children).forEach(function(entry) {
+                firstFeed.push(entry.innerText);
+            });
+            loadFeed(1,done);
+        });
+
+        it('content changes', function() {
+           Array.from(feed.children).forEach(function(entry,index) {
+               console.log(entry.innerText, firstFeed[index], entry.innerText === firstFeed[index]);
+               expect(entry.innerText === firstFeed[index]).toBe(false);
+            });
+        });
+    });
 }());
