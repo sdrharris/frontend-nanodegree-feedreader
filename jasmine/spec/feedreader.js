0efdf9f97e014949e2b82a930634f466c417d9dd
Shannon Harris <sr60621@gmail.com>
2018-11-10 13:00:21 -0600

    Test for hidden class on toggle menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,18 +54,25 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        //Test to check for the hidden class on the toggle-able menu
+        it('is hidden', function() {
+            const body = document.querySelector('body');
+            expect(body.classList.contains('menu-hidden')).toBe(true);
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
